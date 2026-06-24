0b6f793bbb4696c94573bb67311123108a5dcf41
rajeshrushikeshsahu1947-rgb <rajeshrushikeshsahu1947@gmail.com>
2026-06-24 21:53:37 +0530

    Adding existing code

--- train.py
+++ train.py
@@ -1,4 +1,8 @@
 from ultralytics import YOLO
 
 model=YOLO("yolov8m.pt")
-model.train(data="data_custom.yaml",batch = 8 , imgsz=640, epochs=100, workers=1)
+model.train(data="data_custom.yaml",
+            batch = 8 ,
+            imgsz=640,
+            epochs=100,
+            workers=1)
